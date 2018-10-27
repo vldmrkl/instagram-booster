f40a00ff6fd7d0f0c10213c5ace41ab9cacce653
Volodymyr <klymenkodp@gmail.com>
2018-10-26 22:04:13 -0400

    Add list of accounts

---
+++ constants.js
@@ -0,0 +1,15 @@
+const ACCOUNTS = [
+    'tech_insider',
+    'harvard_business_review',
+    'ted',
+    'techcrunch',
+    'entrepreneur',
+    'forbes',
+    'successmagazine',
+    'businessinsider',
+    'thestartup.co',
+    'foundr',
+    'garyvee'
+];
+
+module.exports = ACCOUNTS;
